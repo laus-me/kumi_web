eba738d9ef175d976b2f14a769eaecb8c0671019
PoYang Chen <poyang31@yahoo.com.tw>
2022-06-04 02:53:05 +0000

    reset with tailwindcss

--- src/utils.js
+++
@@ -1,7 +0,0 @@
-import moment from "moment";
-import "moment-duration-format";
-
-export const transferTimeToHumanize = timeSeconds => {
-  const duration = moment.duration(timeSeconds, "seconds");
-  return duration.format("hh:mm:ss");
-};
